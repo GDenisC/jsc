0c31503d17bbaee9cc16a84f3177703f44285d01
DenisC <92527725+GDenisC@users.noreply.github.com>
2025-01-28 11:49:28 +0700

    add more tests

--- tests/all.js
+++ tests/all.js
@@ -24,13 +24,6 @@ runTest(
 );
 
 runTest(
-    'Class Property',
-    'class Class { prop = null; }',
-    { classDestructuring: {} },
-    'function Class_constructor(self) { var self = { prop: null }; return self; }'
-);
-
-runTest(
     'Class Method',
     'class Class { method() {} }',
     { classDestructuring: {} },
@@ -38,10 +31,10 @@ runTest(
 );
 
 runTest(
-    'Class Methods usage',
-    'class Class { constructor(x) { this.x = x } method() { return this.x; } }',
+    'Class Private Method',
+    'class Class { #method() {} }',
     { classDestructuring: {} },
-    'function Class_constructor(x) { var self = {}; self.x = x; return self; } function Class_method(self) { return self.x }'
+    'function Class_private_method(self) {}'
 );
 
 runTest(
@@ -52,10 +45,59 @@ runTest(
 );
 
 runTest(
-    'Class Static Methods usage',
-    'class Class { static method() { return 123; } }',
+    'Class Private Static Method',
+    'class Class { static #method() {} }',
     { classDestructuring: {} },
-    'function Class_static_method() { return 123; }'
+    'function Class_static_private_method() {}'
+);
+
+runTest(
+    'Class Property',
+    'class Class { prop = null; }',
+    { classDestructuring: {} },
+    'function Class_constructor(self) { var self = { prop: null }; return self; }'
+);
+
+runTest(
+    'Class Private Property',
+    'class Class { #prop = null; }',
+    { classDestructuring: {} },
+    'function Class_constructor(self) { var self = { private_prop: null }; return self; }'
+);
+
+runTest(
+    'Class Static Property',
+    'class Class { static prop = 123; }',
+    { classDestructuring: {} },
+    'var Class_static_prop = 123;'
+);
+
+runTest(
+    'Class Private Static Property',
+    'class Class { static #prop = 123; }',
+    { classDestructuring: {} },
+    'var Class_static_private_prop = 123;'
+);
+
+runTest(
+    'Class x prop + method',
+    'class Class { constructor(x) { this.x = x } method() { return this.x; } }',
+    { classDestructuring: {} },
+    'function Class_constructor(x) { var self = {}; self.x = x; return self; } function Class_method(self) { return self.x; }'
+);
+
+runTest(
+    'Class method outer usage as call',
+    'class Class { method() {} } new Class().method();',
+    { classDestructuring: {} },
+    'function Class_constructor() { var self = {}; return self; } function Class_method(self) {} Class_method(Class_constructor());'
+);
+
+runTest(
+    'Class method outer usage as variable',
+    'class Class { method() {} } let a = new Class(); a.method();',
+    { classDestructuring: {} },
+    'function Class_constructor() { var self = {}; return self; } function Class_method(self) {} let a = Class_constructor(); Class_method(a);'
 );
 
 /*
